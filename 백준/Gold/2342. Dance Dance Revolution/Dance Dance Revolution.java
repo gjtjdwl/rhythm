d88e2cd2dc1c2c605c4a93f7161a2e7a4f553db9
gjtjdwl <149356825+gjtjdwl@users.noreply.github.com>
2026-02-27 16:11:59 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 624 ms, Memory: 61412 KB -BaekjoonHub

---
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+// 2342 dance dance revolution
+class Main {
+    static int[] ddr;
+    static int[][][] dp;
+
+    public static int getCost(int now, int next) {
+        if (now == next) {
+            return 1;
+        }
+        if (now == 0) {
+            return 2;
+        }
+        if (Math.abs(now - next) == 2) {
+            return 4;
+        }
+        return 3;
+    }
+
+    public static int dancing(int idx, int left, int right) {
+        if (idx == ddr.length - 1) {
+            return 0;
+        }
+
+        if (dp[idx][left][right] != -1) {
+            return dp[idx][left][right];
+        }
+
+        int target = ddr[idx];
+        int moveLeft = dancing(idx + 1, target, right) + getCost(left, target);
+        int moveRight = dancing(idx + 1, left, target) + getCost(right, target);
+
+        dp[idx][left][right] = Math.min(moveLeft, moveRight);
+
+        return dp[idx][left][right];
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int left, right;
+        left = right = 0;
+        int idx = st.countTokens();
+        ddr = new int[idx];
+        dp = new int[idx][5][5];
+        for (int i = 0; i < idx; i++) {
+            for (int j = 0; j < 5; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+        for (int i = 0; i < idx; i++) {
+            ddr[i] = Integer.parseInt(st.nextToken());
+        }
+        int ans = dancing(0,0,0);
+        System.out.println(ans);
+    }
+}
